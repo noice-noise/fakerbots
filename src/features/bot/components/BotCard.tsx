55f2c802a227375a4552306ef045779827b1701f
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-08-13 18:24:00 +0800

    chore: change state implementation from react state to reducers

--- src/features/bot/components/BotCard.tsx
+++ src/features/bot/components/BotCard.tsx
@@ -11,68 +11,95 @@ import {
   TagLabel,
   Text,
 } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { Bot } from '../types';
+
+enum BotAction {
+  PREV_STATE,
+  NEXT_STATE,
+  OVERRIDE_STATE,
+  RESET_STATE,
+}
+
+type BotActionType =
+  | { type: BotAction.PREV_STATE }
+  | { type: BotAction.NEXT_STATE }
+  | { type: BotAction.OVERRIDE_STATE }
+  | { type: BotAction.RESET_STATE };
+
+const reducer = (state: Bot, action: BotActionType): Bot => {
+  switch (action.type) {
+    case BotAction.NEXT_STATE:
+      return handleStateLoop(state);
+    case BotAction.OVERRIDE_STATE:
+      return handleNextState(state);
+    case BotAction.RESET_STATE:
+      return state;
+    default:
+      throw new Error('Invalid action type.');
+  }
+};
+
+const handleStateLoop = (bot: Bot): Bot => {
+  const currentState = bot.currentState;
+
+  if (currentState.task) {
+    if (currentState.task?.currentValue >= currentState.task.targetValue) {
+      return handleNextState(bot);
+    }
+
+    const updatedState = {
+      ...bot,
+      currentState: {
+        ...currentState,
+        task: {
+          ...currentState.task,
+          currentValue:
+            currentState.task.currentValue + currentState.task.unitValue,
+        },
+      },
+    };
+
+    return updatedState;
+  }
+
+  return bot;
+};
+
+const handleNextState = (bot: Bot): Bot => {
+  // loop to initial state index if last index is reached
+  const nextStateIndex =
+    bot.currentStateIndex + 1 >= bot.states.length
+      ? 0
+      : bot.currentStateIndex + 1;
+
+  let updatedBot: Bot = {
+    ...bot,
+    currentStateIndex: nextStateIndex,
+    currentState: bot.states[nextStateIndex],
+  };
+
+  return updatedBot;
+};
 
 type BotCardProps = {
   bot: Bot;
 };
 
 export const BotCard = ({ bot }: BotCardProps) => {
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setBotState(handleNextState(botState));
-  };
-
-  const [botState, setBotState] = useState<Bot>(bot);
-
-  const MS_PER_LOOP = 1; // milliseconds per loop
+  const MILLISECONDS_PER_LOOP = 1;
   const [loop, setLoop] = useState<number | null>(null);
+  const [state, dispatch] = useReducer(reducer, bot);
 
   useEffect(() => {
     setTimeout(() => {
       setLoop(new Date().getMilliseconds());
-      setBotState((bot) => handleStateLoop(bot));
-    }, MS_PER_LOOP);
-  }, [loop, botState]);
+      dispatch({ type: BotAction.NEXT_STATE });
+    }, MILLISECONDS_PER_LOOP);
+  }, [loop, state]);
 
-  const handleStateLoop = (bot: Bot): Bot => {
-    const currentState = bot.currentState;
-
-    if (currentState.task) {
-      if (currentState.task?.currentValue >= currentState.task.targetValue) {
-        return handleNextState(bot);
-      } else {
-        return {
-          ...bot,
-          currentState: {
-            ...currentState,
-            task: {
-              ...currentState.task,
-              currentValue:
-                currentState.task.currentValue + currentState.task.unitValue,
-            },
-          },
-        };
-      }
-    } else {
-      return bot;
-    }
-  };
-
-  const handleNextState = (bot: Bot): Bot => {
-    // loop to initial state index if last index is reached
-    const nextStateIndex =
-      bot.currentStateIndex + 1 >= bot.states.length
-        ? 0
-        : bot.currentStateIndex + 1;
-
-    let updatedBot: Bot = {
-      ...bot,
-      currentStateIndex: nextStateIndex,
-      currentState: bot.states[nextStateIndex],
-    };
-
-    return updatedBot;
+  const handleClick = () => {
+    dispatch({ type: BotAction.OVERRIDE_STATE });
   };
 
   return (
@@ -86,7 +113,7 @@ export const BotCard = ({ bot }: BotCardProps) => {
     >
       <Flex align='center'>
         <Heading size='sm' fontWeight='bold'>
-          {botState.name}
+          {state.name}
         </Heading>
         <Spacer />
         <Tag size='sm'>
@@ -96,27 +123,27 @@ export const BotCard = ({ bot }: BotCardProps) => {
       <Divider />
       <Flex justify='space-between'>
         <Text opacity={0.75} fontSize='xs'>
-          {botState.currentState.message}
+          {state.currentState.message}
         </Text>
-        <Badge>{botState.currentState.name}</Badge>
+        <Badge>{state.currentState.name}</Badge>
       </Flex>
       <Box>
         <Progress
           colorScheme='green'
           hasStripe
           isAnimated
-          value={Number(botState.currentState.task.currentValue)}
-          max={botState.currentState.task.targetValue}
+          value={Number(state.currentState.task.currentValue)}
+          max={state.currentState.task.targetValue}
           rounded='lg'
           size='sm'
         />
         <Flex justify='space-between' mt={1}>
-          <Text fontSize='xs'>{botState.currentState.task.currentValue}</Text>
-          <Text fontSize='xs'>{botState.currentState.task.targetValue}</Text>
+          <Text fontSize='xs'>{state.currentState.task.currentValue}</Text>
+          <Text fontSize='xs'>{state.currentState.task.targetValue}</Text>
         </Flex>
       </Box>
 
-      <Button variant={'outline'} size='sm' onClick={(e) => handleClick(e)}>
+      <Button variant={'outline'} size='sm' onClick={() => handleClick()}>
         Override
       </Button>
     </Flex>
