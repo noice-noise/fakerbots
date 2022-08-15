5620a8b2e886ab0eb30929bab6ea2cfc7bd12f83
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-08-15 17:38:54 +0800

    styles: update theme styles

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,10 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+import {
+  extendTheme,
+  StyleFunctionProps,
+  withDefaultColorScheme,
+  type ThemeConfig,
+} from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
@@ -8,11 +14,50 @@ const config: ThemeConfig = {
 const theme = extendTheme(
   { config },
   {
+    styles: {
+      global: (props: StyleFunctionProps) => ({
+        body: {
+          fontFamily: 'body',
+          color: mode('gray.800', 'whiteAlpha.900')(props),
+          bg: mode('white', 'neutral.800')(props),
+          lineHeight: 'base',
+        },
+      }),
+    },
     fonts: {
       heading: `'Poppins', sans-serif`,
       body: `'Poppins', sans-serif`,
     },
-  }
+    colors: {
+      brand: {
+        50: '#fffbeb',
+        100: '#fef3c7',
+        200: '#fde68a',
+        300: '#fcd34d',
+        400: '#fbbf24',
+        500: '#f59e0b',
+        600: '#d97706',
+        700: '#b45309',
+        800: '#92400e',
+        900: '#78350f',
+      },
+      neutral: {
+        50: '#fafafa',
+        100: '#f5f5f5',
+        200: '#e5e5e5',
+        300: '#d4d4d4',
+        400: '#a3a3a3',
+        500: '#737373',
+        600: '#525252',
+        700: '#404040',
+        800: '#000',
+        900: '#171717',
+      },
+    },
+  },
+  withDefaultColorScheme({
+    colorScheme: 'gray',
+  })
 );
 
 export default theme;
